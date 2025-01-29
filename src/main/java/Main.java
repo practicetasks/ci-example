100095a9428cdb88787afa110541d88410d72c31
= <=>
2025-01-29 10:33:41 +0500

    add-feature

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // шифрование
-        // хеширование пароль
+        System.out.println("Сообщение из ветки add-feature");
 
     }
 }
